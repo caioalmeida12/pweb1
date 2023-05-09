e6330c9d87f1e9a2a075cb247d249c0f16660f36
caioalmeida08 <araujo.caio08@aluno.ifce.edu.br>
2023-05-09 19:32:50 -0300

    pratica 03 -adicionei as rotas restantes

--- lista05/app.py
+++ lista05/app.py
@@ -4,11 +4,23 @@ app = Flask(__name__, template_folder="templates")
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('paginas/index.html')
+
+@app.route('/carrinho/')
+def carrinhovazio():
+    return render_template('paginas/carrinho.html', item="Carrinho vazio")
 
 @app.route('/carrinho/<item>')
 def carrinho(item):
-    return render_template('carrinho.html', item=item)
+    return render_template('paginas/carrinho.html', item=item)
+
+@app.route('/sobre')
+def sobre():
+    return render_template('paginas/sobre.html')
+
+@app.route('/contato')
+def contato():
+    return render_template('paginas/contato.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
